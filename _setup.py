03b6459cae63d939b1b2663965803cc3f9e77f65
Jonathan Sundqvist <sundqvist.jonathan@gmail.com>
2021-07-05 09:12:44 +0200

    Try to figure out pyproject.toml

---
+++ _setup.py
@@ -0,0 +1,7 @@
+# setup.py
+from setuptools import setup
+setup(
+    name="pelican-obsidian",
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+)
--- setup.py
+++
@@ -1,3 +0,0 @@
-# setup.py
-from setuptools import setup
-setup(name="pelican-obsidian", use_scm_version=True)
