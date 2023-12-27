0180f425355bfdce2fe4ab4b5a658ed95dd23565
Anthony Herrera <anthonyherrera24@gmail.com>
2023-12-27 16:56:25 -0600

    Added parsing for pages, updated code comments for the next person and updated README to reflect changes

--- pelican/plugins/obsidian/obsidian.py
+++ pelican/plugins/obsidian/obsidian.py
@@ -45,6 +45,12 @@ class ObsidianMarkdownReader(MarkdownReader):
         super().__init__(*args, **kwargs)
 
     def replace_obsidian_links(self, text):
+        """
+        Filters all text and replaces matching links with the correct format for pelican. NOTE - this parses all text.
+        Args: 
+            text: Text for entire page
+        Return: text with links replaced if possible
+        """
         def link_replacement(match):
             filename, linkname = get_file_and_linkname(match)
             path = ARTICLE_PATHS.get(filename)
@@ -66,6 +72,7 @@ class ObsidianMarkdownReader(MarkdownReader):
             else:
                 # don't show it at all since it will be broken
                 link_structure = ''
+
             return link_structure
 
         text = file_re.sub(file_replacement, text)
@@ -73,9 +80,8 @@ class ObsidianMarkdownReader(MarkdownReader):
         return text
 
     def read(self, source_path):
-        """Parse content and metadata of markdown files
-
-        It also changes the links to the acceptable format for pelican
+        """
+        Parse content and metadata of markdown files. Also changes the links to the acceptable format for pelican
         """
 
         self._source_path = source_path
@@ -89,34 +95,48 @@ class ObsidianMarkdownReader(MarkdownReader):
             metadata = self._parse_metadata(self._md.Meta)
         else:
             metadata = {}
+
         return content, metadata
 
 
 def populate_files_and_articles(article_generator):
+    """
+    Populates the ARTICLE_PATHS and FILE_PATHS global variables. This is used to find file paths and article paths after
+    parsing the wililink articles.
+    ARTICLE_PATHS is a dictionary where the key is the filename and the value is the path to the article.
+    FILE_PATHS is a dictionary where the key is the file extension and the value is the path to the file
+
+    Args:
+        article_generator: built in class.
+    Returns: None - sets the ARTICLE_PATHS and FILE_PATHS global variables.
+    """
     global ARTICLE_PATHS
     global FILE_PATHS
 
     base_path = Path(article_generator.path)
     articles = base_path.glob('**/*.md')
+    # Get list of all markdown files
     for article in articles:
         full_path, filename_w_ext = os.path.split(article)
         filename, ext = os.path.splitext(filename_w_ext)
         path = str(full_path).replace(str(base_path), '') + '/'
+
+        # This work on both pages and posts/articles
         ARTICLE_PATHS[filename] = path
 
+
+    # Get list of all other relavant files 
     globs = [base_path.glob('**/*.{}'.format(ext)) for ext in ['png', 'jpg', 'svg', 'apkg', 'gif']]
     files = chain(*globs)
-
     for _file in files:
         full_path, filename_w_ext = os.path.split(_file)
         path = str(full_path).replace(str(base_path), '') + '/'
         FILE_PATHS[filename_w_ext] = path
 
 
-def modify_reader(article_generator):
-    populate_files_and_articles(article_generator)
-    article_generator.readers.readers['md'] = ObsidianMarkdownReader(article_generator.settings)
-
+def modify_generator(generator):
+    populate_files_and_articles(generator)
+    generator.readers.readers['md'] = ObsidianMarkdownReader(generator.settings)
 
 def modify_metadata(article_generator, metadata):
     """
@@ -129,4 +149,8 @@ def modify_metadata(article_generator, metadata):
 
 def register():
     signals.article_generator_context.connect(modify_metadata)
-    signals.article_generator_init.connect(modify_reader)
+    signals.article_generator_init.connect(modify_generator)
+
+    signals.page_generator_context.connect(modify_metadata)
+    signals.page_generator_init.connect(modify_generator)
+
